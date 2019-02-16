ff7f56600e52f5f9d209e9ceb18bf167f6646c30
FoxyHawk <wolfyhawk.insalan@gmail.com>
2019-02-16 22:38:16 +0100

    fix redundancy and add keyboard release/press

--- kernel.c
+++ kernel.c
@@ -19,11 +19,20 @@ int kernel_main()
   virtualterminal_writestring(0, "You are welcome");
   virtualterminal_display(0);
 
+
   virtualterminal_setcolor(0, vga_entry_color(VGA_COLOR_LIGHT_CYAN, VGA_COLOR_BROWN));
+
+  virtualterminal_setcursor(0, 10,9);
+  virtualterminal_writestring(0, "                  ");
   virtualterminal_setcursor(0, 10,10);
-  virtualterminal_writestring(0, " Fox_Kernel ");
-  virtualterminal_setcursor(0, 7,12);
-  virtualterminal_writestring(0, " v0.0.1 C edition \n");
+  virtualterminal_writestring(0, "    Fox_Kernel    ");
+  virtualterminal_setcursor(0, 10,11);
+  virtualterminal_writestring(0, "                  ");
+  virtualterminal_setcursor(0, 10,12);
+  virtualterminal_writestring(0, " v0.0.1 C edition ");
+  virtualterminal_setcursor(0, 10,13);
+  virtualterminal_writestring(0, "                  ");
+  virtualterminal_newline(0);
   virtualterminal_display(0);
 
   while(1) {
