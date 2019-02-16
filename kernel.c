d3eab313a84a8dda49046c4a5a901ce50c180935
FoxyHawk <wolfyhawk.insalan@gmail.com>
2019-02-16 18:11:49 +0100

    added virtual terminals

--- kernel.c
+++ kernel.c
@@ -1,23 +1,28 @@
 #include <idt.h>
 #include <terminal.h>
+#include <virtualterminal.h>
 #include <vga.h>
 
 int kernel_main()
 {
+  virtualterminal_initialize();
   terminal_initialize();
 
   idt_install();
-  terminal_writestring("IDT installed\n");
+  virtualterminal_writestring(0, "IDT installed\n");
+  virtualterminal_display(0);
 
-  terminal_writestring("Hello World!\n");
-  terminal_newline();
-  terminal_writestring("You are welcome");
+  virtualterminal_writestring(0, "Hello World!\n");
+  virtualterminal_newline(0);
+  virtualterminal_writestring(0, "You are welcome");
+  virtualterminal_display(0);
 
-  terminal_setcolor(vga_entry_color(VGA_COLOR_LIGHT_CYAN, VGA_COLOR_BROWN));
-  terminal_setcursor(10,10);
-  terminal_writestring(" Fox_Kernel ");
-  terminal_setcursor(7,12);
-  terminal_writestring(" v0.0.1 C edition \n");
+  virtualterminal_setcolor(0, vga_entry_color(VGA_COLOR_LIGHT_CYAN, VGA_COLOR_BROWN));
+  virtualterminal_setcursor(0, 10,10);
+  virtualterminal_writestring(0, " Fox_Kernel ");
+  virtualterminal_setcursor(0, 7,12);
+  virtualterminal_writestring(0, " v0.0.1 C edition \n");
+  virtualterminal_display(0);
 
   while(1) {
     asm volatile ("hlt");
